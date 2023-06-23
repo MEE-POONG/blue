b16e154a0774b574dd5aa62c9983f793541b4176
BlueEyes112 <osakanashi092@gmail.com>
2023-06-23 16:18:48 +0700

    night

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,7 +1,6 @@
-import './globals.css'
+import '../../assets/scss/globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
-
 
 export default function RootLayout({
   children,
